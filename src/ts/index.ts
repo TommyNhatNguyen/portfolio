94240db71cda24c636aee96947649355830c6b3f
nguyen anh nhat <nguyenanhnhat123456@gmail.com>
2025-04-29 02:23:13 +0700

    feat: done refactor hero section aniamtion

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,30 +1,57 @@
 import { gsap } from "gsap";
+import { Observer } from "gsap/Observer";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 // Make sure window is loaded before running the script
+window.scrollTo({
+  top: 0,
+  behavior: "smooth",
+});
 window.addEventListener("load", () => {
   gsap.registerPlugin(ScrollTrigger);
+  gsap.registerPlugin(Observer);
   /**
      ----------------------------------------------------
-     SECTION HERO 
+     SECTION HERO
      ----------------------------------------------------
     */
+  let heroPortfolioTranslateX = 0;
   const heroSection = document.querySelector("#schero");
   const heroSectionWrapper = heroSection.querySelector(".schero-wrapper");
   const heroTitle = heroSection.querySelector(".schero__top-title");
   const heroDescription = heroSection.querySelector(".schero__top-desc");
   const heroImage = heroSection.querySelector(".schero__profile");
   const heroPortfolio = heroSection.querySelector(".schero__bottom-title");
+  const heroPortfolioObserver = Observer.create({
+    target: window,
+    type: "wheel,touch,pointer",
+    onDown: (data) => {
+      if (heroPortfolioTranslateX > -110) {
+        heroPortfolioTranslateX -= 1;
+      }
+    },
+    onUp: () => {
+      if (heroPortfolioTranslateX < 0) {
+        heroPortfolioTranslateX += 1;
+      }
+    },
+    onChangeY: (self) => {
+      if (heroPortfolioTranslateX <= -110) {
+        self.disable();
+      }
+      gsap.to(heroPortfolio, {
+        x: `${heroPortfolioTranslateX}%`,
+        willChange: "transform",
+      });
+    },
+    preventDefault: true,
+  });
   const heroSectionTimeline = gsap.timeline({
     scrollTrigger: {
       trigger: heroSection,
       start: "top top",
       end: "bottom top",
       scrub: 1,
-      pin: true,
       markers: true,
-      snap: {
-        snapTo: 1,
-      },
     },
   });
   gsap.fromTo(
@@ -45,36 +72,42 @@ window.addEventListener("load", () => {
       ease: "power2.inOut",
     }
   );
-  heroSectionTimeline
-    .to(heroPortfolio, {
-      x: "-110%",
-    })
-    .to(heroSection, {
-      willChange: "transform",
-      transform: "scale(0.8) translateY(10%)",
-      onStart: () => {
-        heroSection.classList.add("--with-border-radius");
-      },
-      onReverseComplete: () => {
-        heroSection.classList.remove("--with-border-radius");
-      },
-    });
+  heroSectionTimeline.to(heroSection, {
+    willChange: "transform",
+    transform: "scale(0.8) translateY(100%)",
+    onStart: () => {
+      heroSection.classList.add("--with-border-radius");
+    },
+    onReverseComplete: () => {
+      heroSection.classList.remove("--with-border-radius");
+      heroPortfolioObserver.enable();
+    },
+  });
   /**
      ----------------------------------------------------
-     SECTION WORK 
+     SECTION WORK
      ----------------------------------------------------
     */
-  console.log(
-    "🚀 ~ window.addEventListener ~ heroSection.clientHeight:",
-    heroSection.clientHeight
-  );
   const workSection = document.querySelector("#scwork");
-  heroSectionTimeline.to(
-    workSection,
-    {
-      willChange: "transform",
-      y: `-${heroSection.clientHeight}px`,
+  const workSectionWrapper = workSection.querySelector(".scwork-wrapper");
+  // const workList = workSection.querySelector(".scwork__list");
+  const workListItems =
+    workSectionWrapper.querySelectorAll(".scwork__list-item");
+  const workListItemTimeline = gsap.timeline({
+    scrollTrigger: {
+      trigger: workSectionWrapper,
+      start: "top top",
+      end: "bottom top",
+      scrub: 1,
     },
-    "<"
-  );
+  });
+  workListItems.forEach((item: HTMLElement, index) => {
+    workListItemTimeline.to(
+      item,
+      {
+        y: Number(item.dataset.speed) * -500,
+      },
+      "<"
+    );
+  });
 });
