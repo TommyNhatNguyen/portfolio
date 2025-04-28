a42b50a91931a3947e94b4cbdeff68b409f541ec
nguyen anh nhat <nguyenanhnhat123456@gmail.com>
2025-04-28 14:01:03 +0700

    feat: done hero section transition done about section

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -2,18 +2,31 @@ import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 // Make sure window is loaded before running the script
 window.addEventListener("load", () => {
+  gsap.registerPlugin(ScrollTrigger);
   /**
      ----------------------------------------------------
      SECTION HERO 
      ----------------------------------------------------
     */
   const heroSection = document.querySelector("#schero");
+  const heroSectionWrapper = heroSection.querySelector(".schero-wrapper");
   const heroTitle = heroSection.querySelector(".schero__top-title");
   const heroDescription = heroSection.querySelector(".schero__top-desc");
   const heroImage = heroSection.querySelector(".schero__profile");
   const heroPortfolio = heroSection.querySelector(".schero__bottom-title");
-  const heroSectionTimeline = gsap.timeline({});
-  gsap.registerPlugin(ScrollTrigger);
+  const heroSectionTimeline = gsap.timeline({
+    scrollTrigger: {
+      trigger: heroSection,
+      start: "top top",
+      end: "bottom top",
+      scrub: 1,
+      pin: true,
+      markers: true,
+      snap: {
+        snapTo: 1,
+      },
+    },
+  });
   gsap.fromTo(
     [heroTitle, heroImage, heroDescription],
     {
@@ -34,26 +47,11 @@ window.addEventListener("load", () => {
   );
   heroSectionTimeline
     .to(heroPortfolio, {
-      scrollTrigger: {
-        trigger: heroSection,
-        start: "top top",
-        end: "bottom top",
-        scrub: 1,
-        markers: true,
-        pin: true,
-      },
-      x: "-105%",
+      x: "-110%",
     })
     .to(heroSection, {
-      scrollTrigger: {
-        trigger: heroSection,
-        start: "top top",
-        end: "bottom top",
-        scrub: 1,
-        markers: true,
-      },
       willChange: "transform",
-      transform: "scale(0.9)",
+      transform: "scale(0.8) translateY(10%)",
       onStart: () => {
         heroSection.classList.add("--with-border-radius");
       },
@@ -61,4 +59,22 @@ window.addEventListener("load", () => {
         heroSection.classList.remove("--with-border-radius");
       },
     });
+  /**
+     ----------------------------------------------------
+     SECTION WORK 
+     ----------------------------------------------------
+    */
+  console.log(
+    "🚀 ~ window.addEventListener ~ heroSection.clientHeight:",
+    heroSection.clientHeight
+  );
+  const workSection = document.querySelector("#scwork");
+  heroSectionTimeline.to(
+    workSection,
+    {
+      willChange: "transform",
+      y: `-${heroSection.clientHeight}px`,
+    },
+    "<"
+  );
 });
