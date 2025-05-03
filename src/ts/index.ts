1063694983bbb105e1c8ecad2d92b9e447c15be5
nguyen anh nhat <nguyenanhnhat123456@gmail.com>
2025-05-03 13:36:13 +0700

    feat: done motto

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -243,4 +243,166 @@ window.addEventListener("load", () => {
         )
     );
   });
+
+  /**
+     ----------------------------------------------------
+     SECTION MOTTO
+     ----------------------------------------------------
+    */
+  const mottoSection = document.querySelector("#scmotto");
+  const mottoItemsContainer = mottoSection.querySelector(".scmotto__images");
+  const mottoItems = mottoItemsContainer.querySelectorAll(
+    ".scmotto__images-item"
+  );
+  mottoItems.forEach((item) => {
+    const direction = Math.random() * 2 - 1;
+    const aspectRatio = item.clientWidth / item.clientHeight;
+    const heightScale = Math.max(Math.random() * 2, 0.2);
+    const newHeight = Math.min(item.clientHeight * heightScale, 300);
+    const newWidth = newHeight * aspectRatio;
+    gsap.set(item, {
+      height: newHeight,
+      width: newWidth,
+      y: direction * 100,
+    });
+  });
+  let loop = horizontalLoop(".scmotto__images-item", {
+    speed: 1,
+    repeat: -1,
+    paddingRight: 25,
+  });
 });
+
+type HorizontalLoopConfig = {
+  speed?: number;
+  repeat?: number;
+  paused?: boolean;
+  reversed?: boolean;
+  paddingRight?: number;
+  snap?: boolean | number;
+};
+
+function horizontalLoop(
+  items: NodeListOf<HTMLElement> | HTMLElement[] | string,
+  config: HorizontalLoopConfig = {}
+): gsap.core.Timeline {
+  const elements: HTMLElement[] = gsap.utils.toArray(items) as HTMLElement[];
+  const length = elements.length;
+  const startX = elements[0].offsetLeft;
+  const times: number[] = [];
+  const widths: number[] = [];
+  const xPercents: number[] = [];
+  let curIndex = 0;
+  const pixelsPerSecond = (config.speed || 1) * 100;
+  const snap =
+    config.snap === false
+      ? (v: number) => v
+      : // @ts-ignore
+        gsap.utils.snap(config.snap || 1);
+  let totalWidth: number;
+  let curX: number;
+  let distanceToStart: number;
+  let distanceToLoop: number;
+  let item: HTMLElement;
+  let i: number;
+
+  const tl = gsap.timeline({
+    repeat: config.repeat,
+    paused: config.paused,
+    defaults: { ease: "none" },
+    onReverseComplete: function () {
+      tl.totalTime(tl.rawTime() + tl.duration() * 100);
+    },
+  });
+
+  gsap.set(elements, {
+    xPercent: (i: number, el: HTMLElement) => {
+      const w = (widths[i] = parseFloat(
+        gsap.getProperty(el, "width", "px") as string
+      ));
+      xPercents[i] = snap(
+        (parseFloat(gsap.getProperty(el, "x", "px") as string) / w) * 100 +
+          (gsap.getProperty(el, "xPercent") as number)
+      );
+      return xPercents[i];
+    },
+  });
+  gsap.set(elements, { x: 0 });
+
+  totalWidth =
+    elements[length - 1].offsetLeft +
+    (xPercents[length - 1] / 100) * widths[length - 1] -
+    startX +
+    elements[length - 1].offsetWidth *
+      (gsap.getProperty(elements[length - 1], "scaleX") as number) +
+    (config.paddingRight || 0);
+
+  for (i = 0; i < length; i++) {
+    item = elements[i];
+    curX = (xPercents[i] / 100) * widths[i];
+    distanceToStart = item.offsetLeft + curX - startX;
+    distanceToLoop =
+      distanceToStart +
+      widths[i] * (gsap.getProperty(item, "scaleX") as number);
+    tl.to(
+      item,
+      {
+        xPercent: snap(((curX - distanceToLoop) / widths[i]) * 100),
+        duration: distanceToLoop / pixelsPerSecond,
+      },
+      0
+    )
+      .fromTo(
+        item,
+        {
+          xPercent: snap(
+            ((curX - distanceToLoop + totalWidth) / widths[i]) * 100
+          ),
+        },
+        {
+          xPercent: xPercents[i],
+          duration:
+            (curX - distanceToLoop + totalWidth - curX) / pixelsPerSecond,
+          immediateRender: false,
+        },
+        distanceToLoop / pixelsPerSecond
+      )
+      .add("label" + i, distanceToStart / pixelsPerSecond);
+    times[i] = distanceToStart / pixelsPerSecond;
+  }
+
+  function toIndex(index: number, vars?: gsap.TweenVars) {
+    vars = vars || {};
+    if (Math.abs(index - curIndex) > length / 2) {
+      index += index > curIndex ? -length : length;
+    }
+    const newIndex = gsap.utils.wrap(0, length, index);
+    let time = times[newIndex];
+    if (time > tl.time() !== index > curIndex) {
+      vars.modifiers = { time: gsap.utils.wrap(0, tl.duration()) };
+      time += tl.duration() * (index > curIndex ? 1 : -1);
+    }
+    curIndex = newIndex;
+    vars.overwrite = true;
+    return tl.tweenTo(time, vars);
+  }
+
+  // @ts-ignore
+  tl.next = (vars?: gsap.TweenVars) => toIndex(curIndex + 1, vars);
+  // @ts-ignore
+  tl.previous = (vars?: gsap.TweenVars) => toIndex(curIndex - 1, vars);
+  // @ts-ignore
+  tl.current = () => curIndex;
+  // @ts-ignore
+  tl.toIndex = (index: number, vars?: gsap.TweenVars) => toIndex(index, vars);
+  // @ts-ignore
+  tl.times = times;
+  tl.progress(1, true).progress(0, true);
+
+  if (config.reversed) {
+    // @ts-ignore
+    tl.vars.onReverseComplete();
+    tl.reverse();
+  }
+  return tl;
+}
